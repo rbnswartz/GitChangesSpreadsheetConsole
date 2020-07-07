811ade856b510a365060a1de81444a4a283e38ed
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2020-07-07 12:02:11 -0400

    Added application

---
+++ GitChangesSpreadsheetConsole/Arguments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CommandLine.Text;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace GitChangesSpreadsheetConsole
+{
+    public class Arguments
+    {
+        [Option("input", Required = true, HelpText = "Source git repository directory")]
+        public string InputDirectory { get; set; }
+        [Option("output", Default = "output", HelpText = "Output directory for the csv files")]
+        public string OutputDirectory { get; set; }
+    }
+}
---
+++ GitChangesSpreadsheetConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using CommandLine;
+using CsvHelper;
+using LibGit2Sharp;
+
+namespace GitChangesSpreadsheetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Arguments>(args).WithParsed<Arguments>(arguments =>
+            {
+                var repo = new Repository(arguments.InputDirectory);
+                var repoStatus = repo.RetrieveStatus().OrderBy(i => i.FilePath);
+
+                if (!Directory.Exists(arguments.OutputDirectory))
+                {
+                    Directory.CreateDirectory(arguments.OutputDirectory);
+                }
+
+                WriteToFile(repoStatus.Where(i => i.State == FileStatus.NewInWorkdir), Path.Join(arguments.OutputDirectory, "newitems.csv"));
+                WriteToFile(repoStatus.Where(i => i.State == FileStatus.ModifiedInWorkdir), Path.Join(arguments.OutputDirectory,"updateditems.csv"));
+                WriteToFile(repoStatus.Where(i => i.State == FileStatus.DeletedFromWorkdir), Path.Join(arguments.OutputDirectory,"deleteditems.csv"));
+            });
+        }
+
+        static void WriteToFile(IEnumerable<StatusEntry> input, string fileName)
+        {
+            using (var file = File.Exists(fileName) ? File.Open(fileName, FileMode.Truncate) : File.Open(fileName, FileMode.OpenOrCreate))
+            {
+                using (var writer = new CsvWriter(new StreamWriter(file)))
+                {
+                    writer.WriteRecords(input.Select(i => new { item = i.FilePath, reason = "" }));
+                }
+            }
+        }
+    }
+}
