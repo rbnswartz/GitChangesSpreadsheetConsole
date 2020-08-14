d7ce2c6f892c28ea45b80ef21eba6c8a66dc5e27
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2020-08-14 09:56:36 -0400

    WIP

--- GitChangesSpreadsheetConsole/Arguments.cs
+++ GitChangesSpreadsheetConsole/Arguments.cs
@@ -9,8 +9,8 @@ namespace GitChangesSpreadsheetConsole
     public class Arguments
     {
         [Option("input", Required = true, HelpText = "Source git repository directory")]
-        public string InputDirectory { get; set; }
+        public string? InputDirectory { get; set; }
         [Option("output", Default = "output", HelpText = "Output directory for the csv files")]
-        public string OutputDirectory { get; set; }
+        public string? OutputDirectory { get; set; }
     }
 }
